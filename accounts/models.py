b08d9ff0de35e762d6a101c7787a233cedbd5a30
AlexandrIzevsk <sasha-sarov-333@rambler.ru>
2025-02-05 23:39:11 +0400

    Bild_Board: change max_length Field code

--- accounts/models.py
+++ accounts/models.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 # Create your models here.
 class OneCode(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='code_user')
-    code = models.CharField(max_length=2)
+    code = models.CharField(max_length=5)
